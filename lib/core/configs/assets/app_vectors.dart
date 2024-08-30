30d0436daa93c6a96f227ab4ce14639c9cf53e1b
MamtaBhatt885 <610mamtabhatt@gmail.com>
2024-08-30 22:07:19 +0545

    splash screen

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -3,6 +3,6 @@ class AppVectors{
 
   static const basePath = 'assets/vectors/';
   static const format = '.svg';
-  static const appLogo = 'logo';
+  static const appLogo = '${basePath}logo$format';
 
 }
--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,4 +1,6 @@
+import 'package:bazaar/core/configs/assets/app_vectors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../core/configs/theme/app_colors.dart';
 
@@ -9,6 +11,11 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
+      body: Center(
+        child: SvgPicture.asset(
+          AppVectors.appLogo
+        ),
+      ),
     );
   }
 }
